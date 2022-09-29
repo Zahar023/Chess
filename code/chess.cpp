81ab66e318d110dff74fa34944b6208a88dd2a81
Zahar023 <77553264+Zahar023@users.noreply.github.com>
2022-09-29 20:38:44 +0200

    struct

--- code/chess.cpp
+++ code/chess.cpp
@@ -16,3 +16,18 @@ enum Figure
 	QEEN,
 	KING,
 };
+struct board
+{
+	int presence; //наличие фигуры на клетке
+	Figure figure; //тип фигуры, если она есть
+	int colorCell; //цвет клетки
+	int colrFigure; //цвет фигуры
+	int xp;
+	int yp; //адресса по иксу и игреку
+	int fn; //номер фигуры на клетке
+};
+struct fi
+{
+	int xx;
+	int yy; //адресса для вывода спрайтов
+};
